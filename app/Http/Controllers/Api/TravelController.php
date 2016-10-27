b74813b6f2ebd28a2c51f2af14658dc09cfb8a30
茶谷 <ie4a@MBP13-3.local>
2016-10-27 12:31:46 +0900

    TravelControllerの追加

---
+++ app/Http/Controllers/Api/TravelController.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Travel;
+use App\TravelDetail;
+use App\Genre;
+use Auth;
+use Hash;
+use Response;
+use Illuminate\Http\Request;
+
+class TravelController extends Controller
+{
+
+    public function searchName($name)
+    {
+
+        $travel = Travel::where('name','like',"%{$name}%")->where('releaseFlg',true)->get();
+
+        return Response::json(
+            array(
+                'status' => 'Success',
+                'travel' => $travel
+            )
+        );
+
+    }
+
+    public function searchGenreASC($name)
+    {
+
+        $genre = Genre::where('name',$name)->first();
+
+        $travel = Travel::where('genre_id',$genre->id)->orderBy('travelPoint','ASC')->get();
+
+        return Response::json(
+            array(
+                'status' => 'Success',
+                'travel' => $travel
+            )
+        );
+
+    }
+
+    public function searchGenreDESC($name)
+    {
+
+        $genre = Genre::where('name',$name)->first();
+
+        $travel = Travel::where('genre_id',$genre->id)->orderBy('travelPoint','DESC')->get();
+
+        return Response::json(
+            array(
+                'status' => 'Success',
+                'travel' => $travel
+            )
+        );
+
+    }
+
+    public function travelDetail($id)
+    {
+        $travel = Travel::where('id',$id)->first();
+
+        if($travel->releaseFlg == false){
+            //公開されてない
+            return Response::json(array('status' => 'Failed'));
+        }
+
+        $detail = TravelDetail::where('travel_id',$travel->id)->get();
+
+        return Response::json(
+            array(
+                'status' => 'Success',
+                'detail' => $detail
+            )
+        );
+
+    }
+
+}
