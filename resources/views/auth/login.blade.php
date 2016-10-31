9249fb99a9f532d836f118dfeaebd03343ddc205
茶谷 <ie4a@MBP13-3.local>
2016-10-31 11:58:56 +0900

    ログイン画面変更()

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,4 +63,62 @@
         </div>
     </div>
 </div>
+
+<div class="container">
+    <h1 class="well">User Login</h1>
+    <div class="col-lg-12 well">
+        <div class="row">
+
+            <div class="col-sm-6 form-group">
+                <label>Email</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email..">
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="col-sm-6 form-group">
+                <label>Password</label>
+                <input id="password" type="password" class="form-control" name="password" placeholder="Password..">
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <button type="button" class="btn btn-block btn-info">Login</button>
+            </div>
+        </div>
+        <div class="or-text">
+            <div class="or-text-row">
+                <div class="or-text-line">
+                    <button type="button" class="btn btn-default btn-circle" disabled="disabled">or</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-primary"><i class="fa fa-facebook"></i>Sign in with Facebook</button>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-danger"><i class="fa fa-google-plus"></i>Sign in with GooglePlus</button>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-info"><i class="fa fa-linkedin"></i>Sign in with Linkedin</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
