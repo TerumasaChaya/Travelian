3570d873bada198a31b3e8c7e9791201bf8500dd
茶谷晃昌 <Owner@Chaya.local>
2016-11-13 11:51:21 +0900

    公開旅管理画面と非公開旅管理画面追加

---
+++ resources/views/admin/travels/public.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.elements.basic')
+
+@section('title')
+    共有記
+@endsection
+
+@section('content-header')
+
+    <div class="row">
+        <!-- Page Header -->
+        <div class="col-lg-12">
+            <h1 class="page-header">Public Travels</h1>
+        </div>
+        <!--End Page Header -->
+    </div>
+
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading ">
+                    <i class="fa fa-unlock fa-fw"></i>Travels
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th class="col-lg-2">Name</th>
+                                <th class="col-lg-1">User Name</th>
+                                <th class="col-lg-1">Genre</th>
+                                <th class="col-lg-1">Thumbnail</th>
+                                <th class="col-lg-1">Prefecture</th>
+                                <th class="col-lg-2">Comment</th>
+                                <th class="col-lg-1">Point</th>
+                                <th class="col-lg-3">Created Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($travels as $travel)
+                                <tr>
+                                    <td>{{$travel->name}}</td>
+                                    <td>
+                                        <a href="/admin/users/{{$travel->users->id}}">
+                                            {{$travel->users->name}}
+                                        </a>
+                                    </td>
+                                    <td>{{$travel->genres->name}}</td>
+                                    <td>
+                                        <img src="{{action(ImageController::class.'@thumbnailImage',['name' => $travel->thumbnail])}}"
+                                             class="img-responsive" style="margin-bottom:30px;">
+                                    </td>
+                                    <td>{{$travel->prefecture}}</td>
+                                    <td>{{$travel->comment}}</td>
+                                    <td>{{$travel->travelPoint}}</td>
+                                    <td>{{$travel->created_at}}</td>
+
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="text-right">
+                {!! $travels->render() !!}
+            </div>
+        </div>
+@endsection
