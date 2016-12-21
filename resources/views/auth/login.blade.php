dab2644baf80b5c55ae337d9f393fd08382832fc
茶谷晃昌 <Owner@Chaya.local>
2016-12-21 19:33:24 +0900

    ログインページのデザイン変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1 class="well">User Login</h1>
-        <div class="col-lg-12 well">
-            <div class="row">
-                <form  role="form" method="POST" id="login" action="{{ url('/login') }}">
-                    {{ csrf_field() }}
-                    <div class="col-sm-6 form-group">
-                        <label>Email</label>
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
-                               placeholder="Email..">
+    <div class="container" id="login-block">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-4">
+                <div class="page-icon-shadow animated bounceInDown"></div>
+                <div class="login-box clearfix animated flipInY">
+                    <div class="page-icon animated bounceInDown">
+                        <i class="glyphicon glyphicon-user"></i>
+                    </div>
+                    <div class="login-logo">
+                        <img src="/webImage/login-logo.png" alt="Company Logo">
+                    </div>
+                    <hr>
+                    <div class="login-form">
+                        <form role="form" method="POST" id="login" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+                            <input id="email" type="email" class="input-field" name="email" value="{{ old('email') }}"
+                                   placeholder="Eメール" required="">
                         @if ($errors->has('email'))
-                            <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('email') }}
+                                </div> <!-- End Error box -->
                         @endif
+                            <input id="password" type="password" class="input-field" name="password"
+                                   placeholder="パスワード"  required=""/>
+                            @if ($errors->has('password'))
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('password') }}
+                                </div> <!-- End Error box -->
+                            @endif
+                            <button type="submit" class="btn btn-login">ログイン</button>
+                        </form>
                     </div>
-                    <div class="col-sm-6 form-group">
-                        <label>Password</label>
-                        <input id="password" type="password" class="form-control" name="password"
-                               placeholder="Password..">
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                        @endif
+                    <div class="login-links">
+                        <a href="/password/reset">
+                            パスワードをお忘れですか？
+                        </a>
+                        <br>
+                        <a href="/register">
+                            アカウントをお持ちではありませんか？
+                        </a>
                     </div>
-                </form>
+                </div>
             </div>
 
-            <div class="row">
-                <div class="col-sm-12 text-center">
-                    <button type="button" id="submit" class="btn btn-block btn-info">Login</button>
-                </div>
-            </div>
-            <div class="or-text">
-                <div class="or-text-row">
-                    <div class="or-text-line">
-                        <button type="button" class="btn btn-default btn-circle" disabled="disabled">or</button>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="btn-group">
-                        <a href="/admin/login">
-                            <button type="button" class="btn btn-primary"><i class="fa fa-user-circle"></i>Admin Account Login
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="btn-group">
-                        <a href="/register">
-                            <button type="button" class="btn btn-danger"><i class="fa fa-registered"></i>User Account Register
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="btn-group">
-                        <a href="/password/reset">
-                            <button type="button" class="btn btn-info"><i class="fa fa-question"></i>Forget Your Password?
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
+
 @endsection
+
 
 @section('js-footer')
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,95 +2,81 @@
 
 @section('content')
 
-<div class="container">
-    <h1 class="well">Register</h1>
-    <div class="col-lg-12 well">
+    <div class="container" id="login-block">
         <div class="row">
-            <form  role="form" method="POST" id="register" action="{{ url('/register') }}">
-                {{ csrf_field() }}
-                <div class="col-sm-6 form-group">
-                    <label>Name</label>
-                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}"
-                           placeholder="Name..">
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('name') }}</strong>
-                    </span>
-                    @endif
+            <div class="col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-4">
+                <div class="page-icon-shadow animated bounceInDown"></div>
+                <div class="login-box clearfix animated flipInY">
+                    <div class="page-icon animated bounceInDown">
+                        <i class="glyphicon glyphicon-pencil"></i>
+                    </div>
+                    <div class="login-logo">
+                        <img src="/webImage/login-logo.png" alt="Company Logo">
+                    </div>
+                    <hr>
+                    <div class="login-form">
+                        <form role="form" method="POST" id="register" action="{{ url('/register') }}">
+                            {{ csrf_field() }}
+                            <input id="name" type="text" class="input-field" name="name" value="{{ old('name') }}"
+                                   placeholder="ユーザー名" required="">
+                            @if ($errors->has('name'))
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('name') }}
+                                </div>
+                                <!-- End Error box -->
+                            @endif
+                            <input id="email" type="email" class="input-field" name="email" value="{{ old('email') }}"
+                                   placeholder="Eメール" required="">
+                            @if ($errors->has('email'))
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('email') }}
+                                </div>
+                                <!-- End Error box -->
+                            @endif
+                            <input id="password" type="password" class="input-field" name="password"
+                                   placeholder="パスワード" required=""/>
+                            @if ($errors->has('password'))
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('password') }}
+                                </div> <!-- End Error box -->
+                            @endif
+                            <input id="password-confirm" type="password" class="input-field"
+                                   name="password_confirmation"
+                                   placeholder="パスワード確認" required="">
+                            @if ($errors->has('password_confirmation'))
+                            <!-- Start Error box -->
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert"> ×</button>
+                                    <h4>Error!</h4>
+                                    {{ $errors->first('password_confirmation') }}
+                                </div> <!-- End Error box -->
+                            @endif
+                            <button type="submit" class="btn btn-login">ユーザー作成</button>
+                        </form>
+                    </div>
+                    <div class="login-links">
+                        <a href="/password/reset">
+                            パスワードをお忘れですか？
+                        </a>
+                        <br>
+                        <a href="/login">
+                            アカウントをお持ちの方はこちら
+                        </a>
+                    </div>
                 </div>
-                <div class="col-sm-6 form-group">
-                    <label>Email</label>
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
-                           placeholder="Email..">
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                    @endif
-                </div>
-                <div class="col-sm-6 form-group">
-                    <label>Password</label>
-                    <input id="password" type="password" class="form-control" name="password"
-                           placeholder="Password..">
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-                </div>
-                <div class="col-sm-6 form-group">
-                    <label>Confirm Password</label>
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                           placeholder="Confirm Password..">
-                    @if ($errors->has('password_confirmation'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                    </span>
-                    @endif
-                </div>
-            </form>
-        </div>
+            </div>
 
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <button type="button" id="submit" class="btn btn-block btn-info">Register</button>
-            </div>
-        </div>
-        <div class="or-text">
-            <div class="or-text-row">
-                <div class="or-text-line">
-                    <button type="button" class="btn btn-default btn-circle" disabled="disabled">or</button>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="btn-group">
-                    <a href="/admin/login">
-                        <button type="button" class="btn btn-primary"><i class="fa fa-user-circle"></i>Admin Account Login
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="btn-group">
-                    <a href="/login">
-                        <button type="button" class="btn btn-danger"><i class="fa fa-registered"></i>User Account Login
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="btn-group">
-                    <a href="/password/reset">
-                        <button type="button" class="btn btn-info"><i class="fa fa-question"></i>Forget Your Password?
-                        </button>
-                    </a>
-                </div>
-            </div>
         </div>
     </div>
-</div>
 
 @endsection
 
